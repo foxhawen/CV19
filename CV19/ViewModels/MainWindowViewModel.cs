40d921b2f9ce67ef4057025283f6a968b1db0f37
ଆଣ୍ଡ୍ରେ ମିଲେଜିକ୍ <andrejmilezik@gmail.com>
2021-12-05 13:24:48 +0300

    Привязка свойства Статус к тексту в статус-баре

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -25,5 +25,19 @@ namespace CV19.ViewModels
         }
 
         #endregion
+
+        #region Status : Status - Статус программы
+
+        /// <summary>Статус программы</summary>
+        private string _Status = "Готов!";
+
+        /// <summary>Статус программы</summary>
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+
+        #endregion
     }
 }
