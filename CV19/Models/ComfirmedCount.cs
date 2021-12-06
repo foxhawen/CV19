a05f0e8168704bff5c03293b3bfbf50a266c96f4
ଆଣ୍ଡ୍ରେ ମିଲେଜିକ୍ <andrejmilezik@gmail.com>
2021-12-06 15:51:33 +0300

    Добовляем модели

---
+++ CV19/Models/ComfirmedCount.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CV19.Models
+{
+    internal struct ComfirmedCount
+    {
+        public DateTime Data { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ CV19/Models/PlaceInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace CV19.Models
+{
+    internal class PlaceInfo
+    {
+        public string Name { get; set; }
+        public Point Location { get; set; }
+        public IEnumerable<ComfirmedCount> Counts { get; set; }
+    }
+}
