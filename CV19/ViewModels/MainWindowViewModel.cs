86eaf886ed57fecd52f385982161c35aece8cf87
ଆଣ୍ଡ୍ରେ ମିଲେଜିକ୍ <andrejmilezik@gmail.com>
2021-12-06 18:03:22 +0300

    Пример с управлением вкладками через модель-представления

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -11,6 +11,20 @@ namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region SelectedPageIndex : int - Номер выбранной вкладки
+
+        /// <summary>Номер выбранной вкладки</summary>
+        private int _SelectedPageIndex;
+
+        /// <summary>Номер выбранной вкладки</summary>
+        public int SelectedPageIndex
+        {
+            get => _SelectedPageIndex;
+            set => Set(ref _SelectedPageIndex, value);
+        }
+
+        #endregion
+
         #region TestDataPoints : IEnumerable<DataPoint> - Тестовый набор донных для визуализации графиков
 
         /// <summary>Тестовый набор донных для визуализации графиков</summary>
@@ -71,6 +85,15 @@ namespace CV19.ViewModels
 
         #endregion
 
+        public ICommand ChangeTabIndexCommand { get; }
+        private bool CanChangeTabIndexCommandExecute(object p) => _SelectedPageIndex >= 0;
+
+        private void OnChangeTabIndexCommandExecuted(object p)
+        {
+            if (p is null) return;
+            SelectedPageIndex += Convert.ToInt32(p);
+        }
+
         #endregion
 
         public MainWindowViewModel()
@@ -80,6 +103,8 @@ namespace CV19.ViewModels
             CloseAplicationCommand =
                 new LambdaCommand(OnCloseAplicationCommandExecuted, CanCloseAplicationCommandExecute);
 
+            ChangeTabIndexCommand =
+                new LambdaCommand(OnChangeTabIndexCommandExecuted, CanChangeTabIndexCommandExecute);
             #endregion
 
             var data_points = new List<DataPoint>((int)(360 / 0.1));
