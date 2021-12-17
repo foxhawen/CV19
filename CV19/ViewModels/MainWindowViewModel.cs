74e012bb243d75ab8baa2cacd2db7a528198ac5b
ଆଣ୍ଡ୍ରେ ମିଲେଜିକ୍ <andrejmilezik@gmail.com>
2021-12-17 17:39:53 +0300

    Применение иерархическогошаблона отображения иерархических данных в визуалном элементе TreeView

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -176,6 +176,21 @@ namespace CV19.ViewModels
                     Surname = $"Фамилия {i}"
                 });
 
+        public DirectoryViewModel DiskRootDir { get; } = new DirectoryViewModel("c:\\");
+
+        #region SelectedDirectory : DirectoryViewModel - Выбранная директория
+
+        /// <summary>Выбранная директория</summary>
+        private DirectoryViewModel _SelectedDirectory;
+
+        /// <summary>Выбранная директория</summary>
+        public DirectoryViewModel SelectedDirectory
+        {
+            get => _SelectedDirectory;
+            set => Set(ref _SelectedDirectory, value);
+        }
+
+        #endregion
 
         /*-------------------------------------------------------------------------------------------------------------*/
 
