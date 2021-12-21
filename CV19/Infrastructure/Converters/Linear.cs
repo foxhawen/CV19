033cda4c5311a4b603c2e1249c89516de1082386
ଆଣ୍ଡ୍ରେ ମିଲେଜିକ୍ <andrejmilezik@gmail.com>
2021-12-21 19:16:47 +0300

    Больше конвертеров хороших и разных!Композитный конвертер

--- CV19/Infrastructure/Converters/Linear.cs
+++ CV19/Infrastructure/Converters/Linear.cs
@@ -1,22 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Text;
+using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace CV19.Infrastructure.Converters
 {
-    /// <summary>Реализация линейного преобразования f(x) = k*x + b</summary>
+    /// <summary>Реализация линейного преобразования f(x) = k*x + b</summary>///
+    [ValueConversion(typeof(double), typeof(double))]
     internal class Linear : Converter
     {
+        [ConstructorArgument("K")]
         public double K { get; set; } = 1;
+
+        [ConstructorArgument("B")]
         public double B { get; set; }
+
+        public Linear() { }
+
+        public Linear(double K) => this.K = K;
+
+        public Linear(double K, double B) : this(K) => this.B = B;
 
         public override object Convert(object v, Type t, object p, CultureInfo c)
         {
             if (v is null) return null;
 
             var x = System.Convert.ToDouble(v, c);
-
             return K * x + B;
         }
 
@@ -25,7 +34,6 @@ namespace CV19.Infrastructure.Converters
             if (v is null) return null;
 
             var y = System.Convert.ToDouble(v, c);
-
             return (y - B) / K;
         }
     }
