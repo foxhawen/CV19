2a7b9ec8579fab807147335293cd64ff00578aae
ଆଣ୍ଡ୍ରେ ମିଲେଜିକ୍ <andrejmilezik@gmail.com>
2021-12-22 19:33:31 +0300

    Расширение разметки для конвертеров

--- CV19/Infrastructure/Converters/Ratio.cs
+++ CV19/Infrastructure/Converters/Ratio.cs
@@ -7,6 +7,7 @@ using System.Windows.Markup;
 
 namespace CV19.Infrastructure.Converters
 {
+    [MarkupExtensionReturnType(typeof(Ratio))]
     internal class Ratio : Converter
     {
         [ConstructorArgument("K")]
@@ -32,6 +33,35 @@ namespace CV19.Infrastructure.Converters
             var x = System.Convert.ToDouble(value, c);
 
             return x / K;
+        }
+    }
+
+    [MarkupExtensionReturnType(typeof(Add))]
+    internal class Add : Converter
+    {
+        [ConstructorArgument("K")]
+        public double B { get; set; } = 1;
+
+        public Add() { }
+
+        public Add(double K) => this.B = K;
+
+        public override object Convert(object value, Type t, object p, CultureInfo c)
+        {
+            if (value is null) return null;
+
+            var x = System.Convert.ToDouble(value, c);
+
+            return x + B;
+        }
+
+        public override object ConvertBack(object value, Type t, object p, CultureInfo c)
+        {
+            if (value is null) return null;
+
+            var x = System.Convert.ToDouble(value, c);
+
+            return x - B;
         }
     }
 }
