91913410e488309d5278ab9cee3a64a05151c0bc
ଆଣ୍ଡ୍ରେ ମିଲେଜିକ୍ <andrejmilezik@gmail.com>
2022-01-04 17:49:41 +0300

    Заготовка сервиса для Web-сервера

---
+++ CV19/Services/HttpListnerWebServerer.cs
@@ -0,0 +1,18 @@
+﻿using CV19.Services.Interfaces;
+
+namespace CV19.Services
+{
+    internal class HttpListnerWebServerer : IWebServerService 
+    {
+        public bool Enabled { get; set; }
+        public void Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
