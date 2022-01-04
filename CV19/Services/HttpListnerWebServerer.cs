9cc538e9f09a968c4f5162e8634a5580003c256f
ଆଣ୍ଡ୍ରେ ମିଲେଜିକ୍ <andrejmilezik@gmail.com>
2022-01-04 18:53:05 +0300

    Простейшая логика отправки данных в браузер

--- CV19/Services/HttpListnerWebServerer.cs
+++ CV19/Services/HttpListnerWebServerer.cs
@@ -1,18 +1,28 @@
-﻿using CV19.Services.Interfaces;
+﻿using System;
+using System.IO;
+using CV19.Services.Interfaces;
+using CV19.Web;
 
 namespace CV19.Services
 {
-    internal class HttpListnerWebServerer : IWebServerService 
+    internal class HttpListnerWebServerer : IWebServerService
     {
-        public bool Enabled { get; set; }
-        public void Start()
-        {
-            throw new System.NotImplementedException();
-        }
+        private readonly WebServer _Server = new WebServer(8080);
 
-        public void Stop()
+        public bool Enabled { get => _Server.Enabled; set => _Server.Enabled = value; }
+        public void Start() => _Server.Start();
+
+        public void Stop() => _Server.Stop();
+
+        public HttpListnerWebServerer() => _Server.RequestReceived += OnRequestReceived;
+        
+
+        private static void OnRequestReceived(object? sender, RequestReceiverEventArgs E)
         {
-            throw new System.NotImplementedException();
+            using var writer = new StreamWriter(E.Context.Response.OutputStream);
+
+            writer.WriteLine("CV-19 Application");
+
         }
     }
 }
