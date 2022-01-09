1ddee2790c32f91766e1bb0ecddf1a1ced29e55c
ଆଣ୍ଡ୍ରେ ମିଲେଜିକ୍ <andrejmilezik@gmail.com>
2022-01-09 16:18:09 +0300

    Добавленыповедения для системных кнопок окна

---
+++ CV19/Infrastructure/Bihaviors/CloseWindow.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.Xaml.Behaviors;
+
+namespace CV19.Infrastructure.Bihaviors
+{
+    class CloseWindow : Behavior<Button>
+    {
+        protected override void OnAttached() => AssociatedObject.Click += OnButtonClick;
+
+        protected override void OnDetaching() => AssociatedObject.Click -= OnButtonClick;
+
+        private void OnButtonClick(object sender, RoutedEventArgs E) =>
+            (AssociatedObject.FintVisualRoot() as Window)?.Close();
+    }
+}
---
+++ CV19/Infrastructure/Bihaviors/MinimizeWindow.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace CV19.Infrastructure.Bihaviors
+{
+    class MinimizeWindow : Behavior<Button>
+    {
+        protected override void OnAttached() => AssociatedObject.Click += OnButtonClick;
+
+        protected override void OnDetaching() => AssociatedObject.Click -= OnButtonClick;
+
+        private void OnButtonClick(object sender, RoutedEventArgs E)
+        {
+            if (!(AssociatedObject.FintVisualRoot() is Window window)) return;
+
+            window.WindowState = WindowState.Minimized;
+        }
+    }
+}
