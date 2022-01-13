8c2eb73346125b255c0547e230b2bb3a763b30f7
ଆଣ୍ଡ୍ରେ ମିଲେଜିକ୍ <andrejmilezik@gmail.com>
2022-01-13 19:57:49 +0300

    Отображение студентов

--- CV19/Services/Students/StudentsManager.cs
+++ CV19/Services/Students/StudentsManager.cs
@@ -10,8 +10,8 @@ namespace CV19.Services.Students
         private readonly StudentsRepository _Students;
         private readonly GroupsRepository _Groups;
 
-        public IEnumerable<Student> Student => _Students.GetAll();
-        public IEnumerable<Group> Group => _Groups.GetAll();
+        public IEnumerable<Student> Students => _Students.GetAll();
+        public IEnumerable<Group> Groups => _Groups.GetAll();
 
         public StudentsManager(StudentsRepository Students, GroupsRepository Groups)
         {
--- CV19/ViewModels/StudentsManagementViewModel.cs
+++ CV19/ViewModels/StudentsManagementViewModel.cs
@@ -1,9 +1,18 @@
-﻿using CV19.ViewModels.Base;
+﻿using System.Collections.Generic;
+using CV19.Models.Decanat;
+using CV19.Services.Students;
+using CV19.ViewModels.Base;
 
 namespace CV19.ViewModels
 {
     class StudentsManagementViewModel : ViewModel
     {
+        private readonly StudentsManager _StudentsManager;
+
+        public IEnumerable<Student> Students => _StudentsManager.Students;
+
+        public IEnumerable<Group> Groups => _StudentsManager.Groups;
+
         #region Title : string - Заголовок окна
 
         /// <summary>Заголовок окна</summary>
@@ -17,5 +26,21 @@ namespace CV19.ViewModels
         }
 
         #endregion
+
+        #region SelectedGroup : Group - Выбранная группа студентов
+
+        /// <summary>Выбранная группа студентов</summary>
+        private Group _SelectedGroup;
+
+        /// <summary>Выбранная группа студентов</summary>
+        public Group SelectedGroup
+        {
+            get => _SelectedGroup;
+            set => Set(ref _SelectedGroup, value);
+        }
+
+        #endregion
+
+        public StudentsManagementViewModel(StudentsManager StudentsManager) => _StudentsManager = StudentsManager;
     }
 }
